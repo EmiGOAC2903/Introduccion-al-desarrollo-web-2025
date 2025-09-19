8d620d1496093bc7db74f2da4ddaba8dfe2df23d
Emilio_GA <74110197+EmiGOAC2903@users.noreply.github.com>
2025-09-19 10:01:07 -0600

    Create Tarea JS.js

---
+++ Tarea JS/Tarea JS.js
@@ -0,0 +1,15 @@
+//Multiplicar 2 número
+//Forma clásica
+function multiplicar(a, b) {
+    return a*b;
+}
+//Forma Arrow Function
+const multiplicarArrow = (a, b) => a*b
+
+//Elevar al cuadrado
+//Forma clásica
+function cuadrado(n) {
+    return n * n;
+}
+//Forma Arrow Function
+const cuadradoArrow = n => n * n;
